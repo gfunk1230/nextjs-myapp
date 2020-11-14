119f372c55779ec9efcfb9c5e71c3f17e450c506
Greg Campoverde <gregc1230@icloud.com>
2020-11-14 17:41:21 -0500

    got graphql working with static data

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,82 +1,58 @@
-const apiCall = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res( {
-        testingData: 'hello testing portfolio initial props',
-      });
-    }, 5000)
-  })
-}
+import axios from "axios";
+import PortfolioCard from "@/components/portfolios/PortfolioCard";
+import Link from "next/link";
 
-const Portfolios = ({testingData}) => {
+const fetchPortfolios = () => {
+  const query = `
+    query Portfolios {
+      portfolios {
+        _id,
+        title,
+        company,
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
+        endDate
+      }
+    }`;
+  return axios
+    .post("http://localhost:3000/graphql", { query })
+    .then(({ data: graph }) => graph.data)
+    .then((data) => data.portfolios);
+};
+
+const Portfolios = ({ portfolios }) => {
   return (
     <>
       <section className="section-title">
         <div className="px-2">
           <div className="pt-5 pb-4">
-            {testingData}
-            <h1>Portfolios </h1>
+            <h1>Portfolios</h1>
           </div>
         </div>
       </section>
       <section className="pb-5">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
+          {portfolios.map((portfolio) => (
+            <div key={portfolio._id} className="col-md-4">
+              <Link href="/portfolios/[id]" as={`/portfolios/${portfolio._id}`}>
+                <a className="card-link">
+                  <PortfolioCard portfolio={portfolio} />
+                </a>
+              </Link>
             </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
   );
 };
 
-// Portfolios.getInitialProps = async () => {
-//   const data = await apiCall();
-//   return {
-//     ...data
-//   };
-// }
+Portfolios.getInitialProps = async () => {
+  const portfolios = await fetchPortfolios();
+  return { portfolios };
+};
 
 export default Portfolios;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,95 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+
+const data = {
+  portfolios: [
+    {
+      _id: "sad87da79",
+      title: 'Job in Netcentric',
+      company: 'Netcentric',
+      companyWebsite: 'www.google.com',
+      location: 'New York, New York',
+      jobTitle: 'Engineer',
+      description: 'Doing something, programing....',
+      startDate: '01/01/2014',
+      endDate: '01/01/2016'
+    },
+    {
+      _id: "da789ad1",
+      title: 'Job in Siemens',
+      company: 'Siemens',
+      companyWebsite: 'www.google.com',
+      location: 'Berlin, Germany',
+      jobTitle: 'Software Engineer',
+      description: 'Responsoble for parsing framework for JSON medical data.',
+      startDate: '01/01/2011',
+      endDate: '01/01/2013'
+    },
+    {
+      _id: "sadcxv9",
+      title: 'Work in USA',
+      company: 'WhoKnows',
+      companyWebsite: 'www.google.com',
+      location: 'Sweden, Stockholm',
+      jobTitle: 'Housekeeping',
+      description: 'So much responsibility....Overloaaaaaad',
+      startDate: '01/01/2010',
+      endDate: '01/01/2011'
+    }
+  ]
+}
 
 app.prepare().then(() => {
   const server = express()
+
+  // Construct a schema, using GRAPHQL schema language
+  const schema = buildSchema(`
+      type Porfolio {
+        _id: ID,
+        title: String,
+        company: String,
+        companyWebsite: String,
+        location: String,
+        jobTitle: String,
+        description: String,
+        startDate: String,
+        endDate: String
+      }
+      
+      type Query {
+        hello: String
+        portfolio(id: ID): Porfolio
+        portfolios: [Porfolio]
+      }
+  `);
+
+  // The root provides a resolver for each API endpoint
+  const root = {
+    hello: () => {
+      return 'Hello World!'
+    },
+    portfolio: ({id}) => {
+      const portfolio = data.portfolios.find((p) => p._id === id )
+      return portfolio;
+    },
+    portfolios: () => {
+      return data.portfolios
+    }
+  }
+
+  server.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }));
 
   server.all('*', (req, res) => {
     return handle(req, res)
